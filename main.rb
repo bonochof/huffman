21fd0d74fac34816a47f78a3ab00973972dd6cc1
bonochof <ryos427@yahoo.co.jp>
2017-11-09 17:58:05 +0900

    木の仕様を変更し，ペア作成部分を一部追加

--- main.rb
+++ main.rb
@@ -9,15 +9,20 @@ input.each{ |line| line.strip! }
 
 # init tree
 tree = Array.new
-tree.push( Array.new )
 input.each do |pair|
   val = pair.split
-  tree[0].push( [val[0], val[1].to_f, nil, nil] )
+  tree.push( [val[0], val[1].to_f, nil, nil] )
 end
 
 # sort tree
 tree[0].sort! do |a, b|
   b[1] <=> a[1]
 end
+
+# make pair
+pair = Array.new( 2 )
+pair[0] = tree[-1]
+pair[1] = tree[-2]
+tree[1].push( ["pair0", pair[0][1]+pair[1][1], pair[0][0], pair[1][0]] )
 
 p tree
