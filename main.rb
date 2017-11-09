a3ac59e26722c57fd0580e3532bfa6ec5ec2c3b9
bonochof <ryos427@yahoo.co.jp>
2017-11-09 18:26:57 +0900

    木の仕様変更とペア作成の処理を追加

--- main.rb
+++ main.rb
@@ -14,15 +14,23 @@ input.each do |pair|
   tree.push( [val[0], val[1].to_f, nil, nil] )
 end
 
-# sort tree
-tree[0].sort! do |a, b|
-  b[1] <=> a[1]
-end
-
-# make pair
 pair = Array.new( 2 )
-pair[0] = tree[-1]
-pair[1] = tree[-2]
-tree[1].push( ["pair0", pair[0][1]+pair[1][1], pair[0][0], pair[1][0]] )
+2.times do |i|
+  # sort tree
+  tree.sort_by! do |node|
+    node[1]
+  end
+  
+  # make pair
+  pair[0] = Array.new( tree[0] )
+  pair[1] = Array.new( tree[1] )
+  
+  # change prob
+  tree[0][1] = 1.0
+  tree[1][1] = 1.0
+  
+  # push node
+  tree.push( ["pair#{i}", pair[0][1]+pair[1][1], pair[0][0], pair[1][0]] )
+end
 
 p tree
